313fb10bbf0538abb28bff8487332500dd5c9b3f
yevgeny shuletov <yers@yandex.ru>
2016-02-23 15:56:55 +0300

    second commit

---
+++ frontend/controllers/Mytest3Controller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
+
+class Mytest3Controller extends Controller
+{
+    public function actionIndex()
+    {  
+      return $this->render('about');
+     // echo 'mytest3 - ok';
+ 
+    }
+
+}
